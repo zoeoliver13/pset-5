0d0b34cfafcb5e45c8c4fa6fc9be0e04e9d84d28
Zoe O <zoeo@Zoes-MBP.fios-router.home>
2019-12-08 14:06:01 -0500

    html file isnt working

--- js/shapes.js
+++ js/shapes.js
@@ -19,102 +19,186 @@ window.onload = function() {
     // equivalent set of six event listeners for your solutions. the first one is done for you.
 
     document.getElementById("hello").onclick = sayHello;
-    document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
+    document.getElementById("rectangle").onclick = drawRectangle;
+    document.getElementById("colored_rectangle").onclick = drawColoredRectangle;
+    document.getElementById("triangle").onclick = drawTriangle;
+    document.getElementById("smile").onclick = drawFace;
+    document.getElementById("pyramid").onclick = drawPyramid;
 }
 
 /*
  * Exercise 1.
  */
+
  const sayHello = function() {
-  let draw = document.getElementById("student-canvas-1");
-  let content = draw.getContext("2d");
-  content.clearRect(0, 0, draw.width, draw.height);
-  let message = "";
-  let valid = true;
-    while(valid ==true){
-      message = window.prompt("Message: ", "");
-      if(message.length > 50){
-        alert("Try keeping your message under 50 characters :D");
-      }else{
-        valid = false;
+    let draw = document.getElementById("student-canvas-1");
+    let ctx = draw.getContext("2d");
+    ctx.clearRect(0, 0, draw.width, draw.height);
+    let message = "";
+    let valid = true;
+      while(valid ==true){
+        message = window.prompt("Message: ", "");
+        if(message.length > 50){
+          alert("Try keeping your message under 50 characters :D");
+        }else{
+          valid = false;
+        }
+      }
+      ctx.font = "48px sans-serif";
+      ctx.strokeText(message, 30, 70, 994);
+  };
+
+ /*
+  * Exercise 2.
+  */
+
+  const drawRectangle = function() {
+    let draw = document.getElementById("student-canvas-2");
+    let ctx = draw.getContext("2d");
+    ctx.clearRect(0, 0, draw.width, draw.height);
+    let x = prompt("X: ");
+    let y = prompt("Y: ");
+    let width = prompt("Width: ");
+    let height = prompt("Height: ");
+    while( x < 1 || x > 1024 || Number.isNaN(x) || Number.isNaN(y) || height < 1  y < 1 || y > 512 || width > 1024 || width < 1 || width > 1024 || width < 1 ||  isNaN(width) || height > 512 ||  isNaN(height)) {
+      if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
+        alert("Make sure you're only entering numbers.")
+      }else if (x < 1 || x > 1024) {
+       alert("Make sure your x is > 1 and < 1024.")
+      }else if (y < 1 || y > 512) {
+       alert("Make sure your y is > 1 and < 512.")
+      }else if (width < 1 || width > 1024) {
+       alert("Make sure your width is > 1 and < 1024.")
+      }else if (height < 1 || height > 512) {
+      alert("Make sure your height is > 1 and < 512.")
+      ctx.rect(x, y, width, height);
+      ctx.stroke();
       }
     }
-    content.font = "48px sans-serif";
-    content.strokeText(message, 30, 70, 940);
-    };
+  };
 
-/*
- * Exercise 2.
- */
 
-const drawRectangle = function() {
-    // write your exercise 2 code here
-};
+ /*
+  * Exercise 3.
+  */
+ const drawColoredRectangle = function() {
+    let draw = document.getElementById("student-canvas-3");
+    let ctx = draw.getContext("2d");
+      ctx.clearRect(0, 0, draw.width, draw.height);
 
-/*
- * Exercise 3.
- */
-
-const drawColoredRectangle = function() {
-  let draw = document.getElementById("student-canvas-3");
-  let content = draw.getContext("2d");
-    content.clearRect(0, 0, draw.width, draw.height);
-
-  let color = "";
-  let works = true;
-    while(works == true){
-      color = window.prompt("Enter a color: ")
+    let color = "";
+    if(color==null){
+      break;
     }
-      switch(color){
-        case "black":
-          content.fillStyle = "black";
-          break;
-        case "blue":
-          content.fillStyle = "blue";
-          break;
-        case "green":
-          content.fillStyle = "green";
-          break;
-        case "orange":
-          content.fillStyle = "orange";
-          break;
-        case "purple":
-          content.fillStyle = "purple";
-          break;
-        case "red":
-          content.fillStyle = "red";
-          break;
-        case "yellow":
-          content.fillStyle = "yellow";
-          break;
-    }
-  }
+    let works = true;
+      while(works == true){
+        color = window.prompt("Enter a color: ");
+        color = color.toLowerCase();
+        color = color.trim();
+      }
+        switch(color){
+          case "black":
+            ctx.fillStyle = "black";
+            works = false;
+            break;
+          case "blue":
+            ctx.fillStyle = "blue";
+            works = false;
+            break;
+          case "green":
+            ctx.fillStyle = "green";
+            works = false;
+            break;
+          case "orange":
+            ctx.fillStyle = "orange";
+            works = false;
+            break;
+          case "purple":
+            ctx.fillStyle = "purple";
+            works = false;
+            break;
+          case "red":
+            ctx.fillStyle = "red";
+            works = false;
+            break;
+          case "yellow":
+            ctx.fillStyle = "yellow";
+            works = false;
+            break;
+          default:
+            alert("Try another color. ");
+            works = false;
+            break;
+      }
+        ctx.moveTo(10,10);
+        ctx.rect(100,100,50,50);
+        ctx.stroke();
+  };
+ /*
+  * Exercise 4.
+  */
 
-      content.moveTo(10,10);
-      content.rect(100,100,50,50);
-      content.stroke();
-};
+ const drawTriangle = function() {
+    let draw = document.getElementById("student-canvas-4");
+    let ctx = draw.getContext("2d");
+      ctx.clearRect(0, 0, draw.width, draw.height);
+    let short = Number(prompt("Height: "));
+    let other = Number(prompt("Base: "));
+    let long = Number(prompt("Hypotenuse: "));
 
-/*
- * Exercise 4.
- */
+  while (short*short + other*other != long*long || Number.isNaN(short) || Number.isNaN(other) || Number.isNaN(long) || short > 1024 || other > 1024 || long > 1024))
+        if (short*short + other*other != long*long) {
+          alert("This isn't a right triangle.")
+        }else if(short > 1024 || other > 1024 || long > 1024) {
+        alert("Out of bounds.")
+        }else if((Number.isNaN(short) || Number.isNaN(other) || Number.isNaN(long)) {
+        alert("Make sure all of your sides are numbers.")
+      }
+     short += 25
+     other += 25
+     ctx.beginPath();
+     ctx.moveTo(25, 25);
+     ctx.lineTo(25, short);
+     ctx.lineTo(other, short);
+     ctx.lineTo(25, 25);
+     ctx.stroke();
+  };
 
-const drawTriangle = function() {
-    // write your exercise 4 code here
-};
+ /*
+  * Exercise 5.
+  */
 
-/*
- * Exercise 5.
- */
+ const drawFace = function() {
+   let draw = document.getElementById('student-canvas-5');
+   let ctx = canvas.getContext('2d');
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   let x = draw.width/2;
+   let y = draw.height/2;
+   let radius = (prompt("Radius: "))
+     if (radius == null) {
+       break;
+   while (radius > 256 || Number.isNaN(radius) || radius < 32)
+     }
+     if (radius > 256) {
+       alert("Out of bounds. ")
+     }
+     else if (Number.isNaN(radius)) {
+       alert("Make sure your radius is a number")
+     }
+     else if (radius < 32) {
+       alert("Your radius has to be < 32.")
+     }else{
+       ctx.beginPath();
+       ctx.arc(x, y, radius, 0, 2*Math.PI);
+       ctx.moveTo(x - radius*.25, y - radius*.4);
+       ctx.arc(x - radius*.4, y - radius*.4, radius*.15, 0, 2*Math.PI);
+       ctx.moveTo(x+radius*.55, y - radius*.4);
+       ctx.arc(x + radius*.4, y - radius*.4, radius*.15, 0, 2*Math.PI);
+       ctx.moveTo(x+radius*.7, y);
+       ctx.arc(x, y, radius*.7, 0, Math.PI);
+       ctx.stroke();
+      };
 
-const drawFace = function() {
-    // write your exercise 4 code here
-};
-
-/*
- * Exercise 6 (extra credit).
- */
-
-const drawPyramid = function() {
-    // write your exercise 5 code here
-};
+ /*
+  * Exercise 6 (extra credit).
+  */
